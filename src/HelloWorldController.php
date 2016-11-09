57738cff2e8068c76abc975b43e64639bb9229c4
samson <samson.leung@lalamove.com>
2016-11-09 12:14:09 +0800

    Initial commit.

---
+++ src/HelloWorldController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Sosamson\HelloWorld;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests;
+
+class HelloWorldController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     * 
+     * @return \Illuminate\Http\Response
+     */
+
+     public function index()
+     {     
+         $message = 'Hello World';
+	 return view('HelloWorld::welcome', compact('message'));
+     }
+
+}
